966260d36f819730c00ee86770dadc347747fe4e
rebus_vs <rebus_vs@mail.ru>
2016-02-09 18:16:19 +0300

    fix

--- django_mptt_admin/static/django_mptt_admin/django_mptt_admin.js
+++ django_mptt_admin/static/django_mptt_admin/django_mptt_admin.js
@@ -4,7 +4,7 @@ function initTree($tree, autoopen, autoescape) {
     function createLi(node, $li) {
         // Create edit link
         var $title = $li.find('.jqtree-title');
-        $title.after('<a href="'+ node.url +'" class="edit">' + gettext("(edit)") + '</a>');
+        $title.after('<a href="'+ node.url +'" class="edit"></a>');
     }
 
     function handleMove(e) {
